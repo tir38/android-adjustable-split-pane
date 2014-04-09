62f4ad26ae5c1309ee340f6bd28f64430d971d3a
Jason Atwood <jatwood@bignerdranch.com>
2014-04-09 17:14:00 -0400

    wires up array adapter

--- AdjustableSplitPane/src/com/tir38/android/AdjustableSplitPane/MyListFragment.java
+++ AdjustableSplitPane/src/com/tir38/android/AdjustableSplitPane/MyListFragment.java
@@ -4,8 +4,15 @@ import android.app.Fragment;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.List;
 
 public class MyListFragment extends ListFragment {
+
+
+    private List<Email> mEmails;
 
     public static Fragment newInstance() {
         Log.d("MyListFragment", "inside newInstance");
@@ -17,5 +24,13 @@ public class MyListFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("MyListFragment", "inside onCreate");
+
+        mEmails = DataStore.get().getEmails();
+
+
+        // create array adapter
+        ArrayAdapter<Email> adapter = new ArrayAdapter<Email>(getActivity(), android.R.layout.simple_list_item_1, mEmails);
+
+        setListAdapter(adapter);
     }
 }
