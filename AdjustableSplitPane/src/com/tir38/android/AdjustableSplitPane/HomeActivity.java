da3666b92e3a58aa6a68979395df56fd0a63f3dc
Jason Atwood <jatwood@bignerdranch.com>
2014-04-09 19:29:42 -0400

    adds home activity to manage switching between "regular" and split pane

---
+++ AdjustableSplitPane/src/com/tir38/android/AdjustableSplitPane/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.tir38.android.AdjustableSplitPane;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button regularButton = (Button) findViewById(R.id.activity_home_regular_button);
+        regularButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MyListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button splitPaneButton = (Button) findViewById(R.id.activity_home_split_pane_button);
+        splitPaneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MySplitPaneActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
